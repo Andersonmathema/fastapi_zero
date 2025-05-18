8d442fec80ef5a7fb5325f4087d131f982b367e9
Anderson Andrade Siva <ander.math89@gmail.com>
2025-05-17 22:04:45 -0300

    Aula 02 e 03: entre aulas

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -2,15 +2,26 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fastapi_zero.schemas import Message
+from fastapi_zero.schemas import Message, UserSchema, UserPublic, UserDB
 
-app = FastAPI()
+app = FastAPI(title='Minha API')
 
+database = []
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'menssagem': 'Olá Mundo!'}
+    return {'message': 'Olá Mundo!'}
 
-    #return {'message': 'Olá Mundo!'}
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        username= user.username,
+        email=user.email,
+        password=user.password,
+        id= len(database) + 1
+    )
+    
+    database.append(user_with_id)
 
+    return user_with_id 
 
