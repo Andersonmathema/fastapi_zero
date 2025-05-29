e5e82872907c226d786c759ad4e466362fa5b3f5
Anderson Andrade Silva <ander.math89@gmail.com>
2025-05-29 17:18:10 -0300

    Aula 05: Exercicios feitos

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -7,14 +7,9 @@ from sqlalchemy.orm import Session
 
 from fastapi_zero.database import get_session
 from fastapi_zero.models import User
-from fastapi_zero.schemas import (
-    Message,
-    UserList,
-    UserPublic,
-    UserSchema,
-)
+from fastapi_zero.schemas import Message, UserList, UserPublic, UserSchema
 
-app = FastAPI(title='Minha API')
+app = FastAPI()
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -26,11 +21,11 @@ def read_root():
 def create_user(user: UserSchema, session: Session = Depends(get_session)):
     db_user = session.scalar(
         select(User).where(
-            User.username == user.username or User.email == user.email
+            (User.username == user.username) | (User.email == user.email)
         )
     )
+
     if db_user:
-        # Retornar erro
         if db_user.username == user.username:
             raise HTTPException(
                 status_code=HTTPStatus.CONFLICT,
@@ -38,12 +33,12 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
             )
         elif db_user.email == user.email:
             raise HTTPException(
-                status_code=HTTPStatus.CONFLICT, detail='Email already exists'
+                status_code=HTTPStatus.CONFLICT,
+                detail='Email already exists',
             )
 
-    # Se não der erro
     db_user = User(
-        username=user.username, email=user.email, password=user.password
+        username=user.username, password=user.password, email=user.email
     )
 
     session.add(db_user)
@@ -53,66 +48,64 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
     return db_user
 
 
-@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+@app.get('/users/', response_model=UserList)
 def read_users(
-    limit: int = 10, offset: int = 0, session: Session = Depends(get_session)
+    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
 ):
-    users = session.scalars(select(User).limit(limit).offset(offset))
+    users = session.scalars(select(User).offset(skip).limit(limit)).all()
     return {'users': users}
 
 
-@app.put(
-    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
-)
+@app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(
     user_id: int, user: UserSchema, session: Session = Depends(get_session)
 ):
-    user_db = session.scalar(select(User).where(User.id == user_id))
-
-    if not user_db:
+    db_user = session.scalar(select(User).where(User.id == user_id))
+    if not db_user:
         raise HTTPException(
-            detail='User not found',
-            status_code=HTTPStatus.NOT_FOUND,
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
-    try:
-        user_db.email = user.email
-        user_db.username = user.username
-        user_db.password = user.password
 
-        session.add(user_db)
+    try:
+        db_user.username = user.username
+        db_user.password = user.password
+        db_user.email = user.email
         session.commit()
-        session.refresh(user_db)
-        return user_db
+        session.refresh(db_user)
+
+        return db_user
+
     except IntegrityError:
         raise HTTPException(
-            detail='Username or email already exists',
             status_code=HTTPStatus.CONFLICT,
+            detail='Username or Email already exists',
         )
 
 
 @app.delete('/users/{user_id}', response_model=Message)
 def delete_user(user_id: int, session: Session = Depends(get_session)):
-    user_db = session.scalar(select(User).where(User.id == user_id))
+    db_user = session.scalar(select(User).where(User.id == user_id))
 
-    if not user_db:
+    if not db_user:
         raise HTTPException(
-            detail='User not found',
-            status_code=HTTPStatus.NOT_FOUND,
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
 
-    session.delete(user_db)
+    session.delete(db_user)
     session.commit()
 
     return {'message': 'User deleted'}
 
 
-# @app.get(
-#     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
-# )
-# def get_one_user(user_id: int):
-#     if user_id < 1 or user_id > len(database):
-#         raise HTTPException(
-#             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
-#         )
+@app.get('/users/{user_id}', response_model=UserPublic)
+def read_user__exercicio(
+    user_id: int, session: Session = Depends(get_session)
+):
+    db_user = session.scalar(select(User).where(User.id == user_id))
 
-#     return database[user_id - 1]
+    if not db_user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    return db_user
--- tests/test_app.py
+++ tests/test_app.py
@@ -75,7 +75,7 @@ def test_update_integrity_error(client, user):
     )
 
     assert response.status_code == HTTPStatus.CONFLICT
-    assert response.json() == {'detail': 'Username or email already exists'}
+    assert response.json() == {'detail': 'Username or Email already exists'}
 
 
 def test_delete_user(client, user):
@@ -96,28 +96,48 @@ def test_update_user_should_return_not_found__exercicio(client):
     )
 
     assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'User not found!'}
-
-
-def test_get_user_should_return_not_found__exercicio(client):
-    response = client.get('/users/666')
-
-    assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not found'}
-
-
-def test_get_user___exercicio(client):
-    response = client.get('/users/1')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'username': 'bob',
-        'email': 'bob@example.com',
-        'id': 1,
-    }
 
 
 def test_delete_user_should_return_not_found__exercicio(client):
     response = client.delete('/users/666')
+
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not found'}
+
+
+def test_get_user___exercicio(client, user):
+    response = client.get(f'/users/{user.id}')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': user.username,
+        'email': user.email,
+        'id': user.id,
+    }
+
+
+def test_create_user_should_return_409_username_exists__exe(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': user.username,
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Username already exists'}
+
+
+def test_create_user_should_return_409_email_exists__exe(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': user.email,
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Email already exists'}
